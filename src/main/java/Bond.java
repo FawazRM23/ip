f5848a37832c9645448ce4a4596dcc3f0de1fb3f
Reyas Mohideen Fawaz <rmfawaz07@gmail.com>
2026-08-28 02:11:03 +0800

    Add initial Bond greeting and farewell
    Display the chatbot banner, greet the user, and exit with a farewell message as required.

--- src/main/java/Bond.java
+++ src/main/java/Bond.java
@@ -1,10 +1,23 @@
+//Greets user and then exits.
 public class Bond {
+
+    //Runs chatbot greeting.
+
     public static void main(String[] args) {
+
+        String divider = "____________________________________________________________";
         String banner = "    ____                  __\n"
                 + "   / __ )____  ____  ____/ /\n"
                 + "  / __  / __ \\/ __ \\/ __  /\n"
                 + " / /_/ / /_/ / / / / /_/ /\n"
                 + "/_____/\\____/_/ /_/\\__,_/\n";
-        System.out.println(banner);
+
+        System.out.println(divider);
+        System.out.print(banner);
+        System.out.println("Good day! I'm Bond, James Bond.");
+        System.out.println("Agent 007 at your service, what can I do for you?");
+        System.out.println(divider);
+        System.out.println("Mission accomplished. Hope we embark on a mission again soon!");
+        System.out.println(divider);
     }
 }
