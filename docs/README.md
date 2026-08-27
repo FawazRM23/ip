073d44a27b9555e1fdd1f45d1d5ebfdf4526cd7a
Reyas Mohideen Fawaz <rmfawaz07@gmail.com>
2026-08-28 01:57:24 +0800

    Rename chatbot from Duke to Bond
    Update the Java entry point, startup banner, and documentation so the project consistently uses the new chatbot name.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Bond User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Bond.java
@@ -0,0 +1,10 @@
+public class Bond {
+    public static void main(String[] args) {
+        String banner = "    ____                  __\n"
+                + "   / __ )____  ____  ____/ /\n"
+                + "  / __  / __ \\/ __ \\/ __  /\n"
+                + " / /_/ / /_/ / / / / /_/ /\n"
+                + "/_____/\\____/_/ /_/\\__,_/\n";
+        System.out.println(banner);
+    }
+}
